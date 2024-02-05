1f60e497d7e15c3931f8d1119bd15194aee2ff78
shaddarion <Sergey.Gaidamaka@advarra.com>
2024-02-05 16:55:23 -0700

    fix tag issue

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function run() {
     try {
         const versionType = core.getInput('version-type');
         const src = __dirname;
- 
+
         await exec.exec(`${src}/git_update.sh -v ${versionType}`);
     } catch (error) {
         core.setFailed(error.Message);
