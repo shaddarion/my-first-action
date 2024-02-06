e554e09425be3b4f3a5b1326bc98b01ab831b7bb
shaddarion <Sergey.Gaidamaka@advarra.com>
2024-02-05 17:02:30 -0700

    fix tag issue

--- action/git_update.sh
+++ action/git_update.sh
@@ -5,17 +5,18 @@ VERSION=""
 # get parameters
 while getopts v: flag
 do
-    case "${flag}" in
-        v) VERSION=${OPTAG};;
-    esac
+  case "${flag}" in
+    v) VERSION=${OPTARG};;
+  esac
 done
 
+# get highest tag number, and add v0.1.0 if doesn't exist
 git fetch --prune --unshallow 2>/dev/null
 CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 
 if [[ $CURRENT_VERSION == '' ]]
 then
-    CURRENT_VERSION='v0.1.0'
+  CURRENT_VERSION='v0.1.0'
 fi
 echo "Current Version: $CURRENT_VERSION"
 
@@ -24,21 +25,21 @@ CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
 
 # get number parts
 VNUM1=${CURRENT_VERSION_PARTS[0]}
-VNUM2=${CURRENT_VERSION_PARTS[2]}
+VNUM2=${CURRENT_VERSION_PARTS[1]}
 VNUM3=${CURRENT_VERSION_PARTS[2]}
 
 if [[ $VERSION == 'major' ]]
 then
-    VNUM1=v$((VNUM1+1))
+  VNUM1=v$((VNUM1+1))
 elif [[ $VERSION == 'minor' ]]
 then
-    VNUM2=$((VNUM2+1))
+  VNUM2=$((VNUM2+1))
 elif [[ $VERSION == 'patch' ]]
 then
-    VNUM3=$((VNUM3+1))
+  VNUM3=$((VNUM3+1))
 else
-    echo "No version type or incorrect type sspecified, try: -v [major, minor, patch]"
-    exit 1
+  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
+  exit 1
 fi
 
 # create new tag
@@ -50,13 +51,13 @@ GIT_COMMIT=`git rev-parse HEAD`
 NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
 # only tag if no tag already
-if [ -z "$NEEDS_TAG"]; then
-    echo "Tagged with $NEW_TAG"
-    git tag $NEW_TAG
-    git push --tags
-    git push
+if [ -z "$NEEDS_TAG" ]; then
+  echo "Tagged with $NEW_TAG"
+  git tag $NEW_TAG
+  git push --tags
+  git push
 else
-    echo "Already a tag on this commit"
+  echo "Already a tag on this commit"
 fi
 
 echo ::set-output name=new-version::$NEW_TAG
